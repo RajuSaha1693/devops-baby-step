4d521592d355d5d304e913d27b9cad7172b9e450
rajusaha010693@gmail.com <rajusaha010693@gmail.com>
2026-01-01 16:50:56 +0530

    minor changes

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from DevOps Pipeline 🚀");
+  res.send("Hello CD automation");
 });
 
 app.get("/health", (req, res) => {
